dbb17f8044c0f6b6ab3e02a09cd9106afa930471
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-06-23 20:07:20 +0530

    testing

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./project.css";
-import IMG1 from "../../assests/project1.jpg";
-import IMG2 from "../../assests/project2.jpg";
+import IMG1 from "../../assests/project1.png";
+import IMG2 from "../../assests/project2.png";
 const Portfolio = () => {
   return (
     <section id="project">
@@ -15,11 +15,11 @@ const Portfolio = () => {
           </div>
           <h3>Personal Portfolio Website</h3>
           <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target="_blank">
+            <a href="https://github.com/ManishK4514/PersonalPortfolio" className="btn" target="_blank">
               Github
             </a>
             <a
-              href="https://github.com"
+              href="https://manishk4514.netlify.app/"
               className="btn btn-primary"
               target="_blank"
             >
